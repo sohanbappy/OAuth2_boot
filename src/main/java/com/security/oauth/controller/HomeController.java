de9dce273ebe9f63055f38118a994b0c5d10acee
bappy <bappy@rokomari.com>
2020-09-20 20:59:03 +0600

    Adding Swagger Info Controller

--- src/main/java/com/security/oauth/controller/HomeController.java
+++ src/main/java/com/security/oauth/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.security.oauth.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +13,14 @@ import java.util.Map;
 //Enabling Swagger UI for root directory access
 //URL/swagger-ui.html   URL/v2/api-docs
 public class HomeController {
+
     @RequestMapping("/user")
+    @ApiOperation(value = "Get User Info from OAuth2",
+    notes = "getting user info of logged in user",
+    response = Map.class)
     public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
         System.out.println(principal);
         return Collections.singletonMap("name", principal.getAttribute("name"));
     }
+
 }
